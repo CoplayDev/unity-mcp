7517d4a4c592d4e5b26c9c234d6dbdd99e585bc3
Jordon Harrison <Jordon.Harrison@outlook.com>
2025-12-05 12:26:25 +0000

    feat: enhance ConfigJsonBuilder to support Authorization header input management

--- MCPForUnity/Editor/Helpers/ConfigJsonBuilder.cs
+++ MCPForUnity/Editor/Helpers/ConfigJsonBuilder.cs
@@ -15,6 +15,7 @@ namespace MCPForUnity.Editor.Helpers
 {
     public static class ConfigJsonBuilder
     {
+        private const string AuthTokenInputKey = "Authorization";
         public static string BuildManualConfigJson(string uvPath, McpClient client)
         {
             var root = new JObject();
@@ -63,85 +64,6 @@ namespace MCPForUnity.Editor.Helpers
                 unity[httpProperty] = httpUrl;
 
                 bool authEnabled = AuthPreferencesUtility.GetAuthEnabled();
-                if (authEnabled)
-                {
-                    // Align with GitHub-style input binding: Authorization header references an input
-                    var headers = unity["headers"] as JObject ?? new JObject();
-                    headers["Authorization"] = "${input:Authorization}";
-                    unity["headers"] = headers;
-                }
-                else
-                {
-                    if (unity["headers"] is JObject headers && headers.ContainsKey("Authorization"))
-                    {
-                        headers.Remove("Authorization");
-                        if (!headers.Properties().Any())
-                        {
-                            unity.Remove("headers");
-                        }
-                        else
-                        {
-                            unity["headers"] = headers;
-                        }
-                    }
-
-                }
-
-                foreach (var prop in urlPropsToRemove)
-                {
-                    if (unity[prop] != null) unity.Remove(prop);
-                }
-
-                // Remove command/args if they exist from previous config
-                if (unity["command"] != null) unity.Remove("command");
-                if (unity["args"] != null) unity.Remove("args");
-
-                if (isVSCode)
-                {
-                    unity["type"] = "http";
-                }
-            }
-            else
-            {
-                // Stdio mode: Use uvx command
-                var (uvxPath, fromUrl, packageName) = AssetPathUtility.GetUvxCommandParts();
-
-                var toolArgs = BuildUvxArgs(fromUrl, packageName);
-
-                if (ShouldUseWindowsCmdShim(client))
-                {
-                    unity["command"] = ResolveCmdPath();
-
-                    var cmdArgs = new List<string> { "/c", uvxPath };
-                    cmdArgs.AddRange(toolArgs);
-
-                    unity["args"] = JArray.FromObject(cmdArgs.ToArray());
-                }
-                else
-                {
-                    unity["command"] = uvxPath;
-                    unity["args"] = JArray.FromObject(toolArgs.ToArray());
-                }
-
-                // Remove url/serverUrl if they exist from previous config
-                if (unity["url"] != null) unity.Remove("url");
-                if (unity["serverUrl"] != null) unity.Remove("serverUrl");
-
-                if (isVSCode)
-                {
-                    unity["type"] = "stdio";
-                }
-            }
-
-            // Remove type for non-VSCode clients
-            if (!isVSCode && unity["type"] != null)
-            {
-                unity.Remove("type");
-            }
-
-            bool requiresEnv = client?.EnsureEnvObject == true;
-            bool stripEnv = client?.StripEnvWhenNotRequired == true;
-
                 if (authEnabled)
                 {
                     // Align with GitHub-style input binding: Authorization header references an input
@@ -199,6 +121,97 @@ namespace MCPForUnity.Editor.Helpers
                         }
                     }
                 }
+
+                foreach (var prop in urlPropsToRemove)
+                {
+                    if (unity[prop] != null) unity.Remove(prop);
+                }
+
+                // Remove command/args if they exist from previous config
+                if (unity["command"] != null) unity.Remove("command");
+                if (unity["args"] != null) unity.Remove("args");
+
+                if (isVSCode)
+                {
+                    unity["type"] = "http";
+                }
+            }
+            else
+            {
+                // Stdio mode: Use uvx command
+                var (uvxPath, fromUrl, packageName) = AssetPathUtility.GetUvxCommandParts();
+
+                var toolArgs = BuildUvxArgs(fromUrl, packageName);
+
+                if (ShouldUseWindowsCmdShim(client))
+                {
+                    unity["command"] = ResolveCmdPath();
+
+                    var cmdArgs = new List<string> { "/c", uvxPath };
+                    cmdArgs.AddRange(toolArgs);
+
+                    unity["args"] = JArray.FromObject(cmdArgs.ToArray());
+                }
+                else
+                {
+                    unity["command"] = uvxPath;
+                    unity["args"] = JArray.FromObject(toolArgs.ToArray());
+                }
+
+                // Remove url/serverUrl if they exist from previous config
+                if (unity["url"] != null) unity.Remove("url");
+                if (unity["serverUrl"] != null) unity.Remove("serverUrl");
+
+                if (isVSCode)
+                {
+                    unity["type"] = "stdio";
+                }
+            }
+
+            // Remove type for non-VSCode clients
+            if (!isVSCode && unity["type"] != null)
+            {
+                unity.Remove("type");
+            }
+
+            bool requiresEnv = client?.EnsureEnvObject == true;
+            bool stripEnv = client?.StripEnvWhenNotRequired == true;
+
+            if (requiresEnv)
+            {
+                if (unity["env"] == null)
+                {
+                    unity["env"] = new JObject();
+                }
+            }
+            else if (stripEnv && unity["env"] != null)
+            {
+                unity.Remove("env");
+            }
+
+            if (client?.DefaultUnityFields != null)
+            {
+                foreach (var kvp in client.DefaultUnityFields)
+                {
+                    if (unity[kvp.Key] == null)
+                    {
+                        unity[kvp.Key] = kvp.Value != null ? JToken.FromObject(kvp.Value) : JValue.CreateNull();
+                    }
+                }
+            }
+        }
+
+        private static JObject EnsureObject(JObject parent, string name)
+        {
+            if (parent[name] is JObject o) return o;
+            var created = new JObject();
+            parent[name] = created;
+            return created;
+        }
+
+        private static IList<string> BuildUvxArgs(string fromUrl, string packageName)
+        {
+            var args = new List<string> { packageName };
 
             if (!string.IsNullOrEmpty(fromUrl))
             {
