49901a3b9a0c62c91b813a21e056f4e638b75804
张洋凡 <378905096@qq.com>
2026-02-28 16:36:01 +0800

    fix namespace

--- MCPForUnity/Editor/Setup/McpForUnitySkillInstaller.cs
+++ MCPForUnity/Editor/Setup/McpForUnitySkillInstaller.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 using UnityEditor;
 using UnityEngine;
 
-namespace Editor
+namespace MCPForUnity.Editor.Setup
 {
     public class McpForUnitySkillInstaller : EditorWindow
     {
